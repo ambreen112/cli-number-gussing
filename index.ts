ce37edb8ea91c65db8e1a0fd54736004b9fe9143
unknown <ambreenjahan144@gmail.com>
2024-03-25 20:54:12 +0500

    push my cli-number gussing

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+const randomNumber= Math.floor(Math.random()*6+1);
+
+const answers = await inquirer.prompt([{
+
+    name: "userGuessedNumber",
+    type: "number",
+    message:"Pleace guess a number between 1 to 6:",
+},
+]);
+
+console.log(answers);
+
+if(answers.userGuessedNumber===randomNumber){
+    console.log("Congratulation ! you guessed right number.");
+}else{
+    console.log("You guessed wrong number");
+}
